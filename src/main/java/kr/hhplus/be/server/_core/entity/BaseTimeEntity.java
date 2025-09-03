6dcb957b43b5185dc5c2aa10e77717a6598c58c5
이창민 <chang6585@wedatalab.com>
2025-09-03 16:16:05 +0900

    feat: 공통 entity getter추가

--- src/main/java/kr/hhplus/be/server/_core/entity/BaseTimeEntity.java
+++ src/main/java/kr/hhplus/be/server/_core/entity/BaseTimeEntity.java
@@ -3,10 +3,12 @@ package kr.hhplus.be.server._core.entity;
 import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseTimeEntity extends IdentifiableEntity {
     @CreationTimestamp
