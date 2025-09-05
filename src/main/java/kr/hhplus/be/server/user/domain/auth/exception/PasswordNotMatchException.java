f6b0943c22cfc3de38c00be479e3276d26a3d705
changm-L <chang6585@naver.com>
2025-09-05 12:05:56 +0900

    chore: IllegalArgumentException -> PasswordNotMatchException

---
+++ src/main/java/kr/hhplus/be/server/user/domain/auth/exception/PasswordNotMatchException.java
@@ -0,0 +1,9 @@
+package kr.hhplus.be.server.user.domain.auth.exception;
+
+public class PasswordNotMatchException extends AuthException {
+    private static final String MESSAGE = "아이디 또는 비밀번호가 올바르지 않습니다.";
+
+    public PasswordNotMatchException() {
+        super(MESSAGE);
+    }
+}
