8d2f0d109ba46de011bbb3a2539fc893bc5f9be9
changm-L <chang6585@naver.com>
2025-09-07 13:29:00 +0900

    refactor: localDate validate 변경

--- src/main/java/kr/hhplus/be/server/admin/domain/concert/dto/request/CreateConcertRequest.java
+++ src/main/java/kr/hhplus/be/server/admin/domain/concert/dto/request/CreateConcertRequest.java
@@ -2,17 +2,14 @@ package kr.hhplus.be.server.admin.domain.concert.dto.request;
 
 import java.time.LocalDate;
 import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PastOrPresent;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateConcertRequest(
         @NotBlank String title,
         @NotBlank String address,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") @FutureOrPresent LocalDate startsAt,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") @PastOrPresent LocalDate endsAt
+        @NotNull LocalDate startsAt,
+        @NotNull LocalDate endsAt
 ) {
 
     @AssertTrue
