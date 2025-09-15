88433b28ba32db44eaa66af09daa3ba26b0becc7
이창민 <chang6585@wedatalab.com>
2025-09-15 15:34:32 +0900

    chore transactional

--- src/main/java/kr/hhplus/be/server/user/domain/concert/service/ConcertService.java
+++ src/main/java/kr/hhplus/be/server/user/domain/concert/service/ConcertService.java
@@ -3,6 +3,7 @@ package kr.hhplus.be.server.user.domain.concert.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.hhplus.be.server.user.domain.concert.dto.response.ConcertDetailResponse;
 import kr.hhplus.be.server.user.domain.concert.dto.response.ConcertListResponse;
@@ -15,10 +16,12 @@ public class ConcertService {
 
     private final ConcertRepository repository;
 
+    @Transactional(readOnly = true)
     public List<ConcertListResponse> findAllConcerts() {
         return repository.findAllConcertWithAvailableSeatsCount();
     }
 
+    @Transactional(readOnly = true)
     public ConcertDetailResponse getConcertById(long concertId) {
         Concert concert = repository.getById(concertId);
         return ConcertDetailResponse.of(concert);
