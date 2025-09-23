278274fce426c223ed20ee0425ecf30d88039a7a
이창민 <chang6585@wedatalab.com>
2025-09-23 14:51:49 +0900

    test: concert, seat controller test 추가

---
+++ src/test/java/kr/hhplus/be/server/user/domain/seat/controller/SeatControllerTest.java
@@ -0,0 +1,60 @@
+package kr.hhplus.be.server.user.domain.seat.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.hhplus.be.server.core.utils.ControllerTest;
+import kr.hhplus.be.server.fixture.concert.ConcertFixture;
+import kr.hhplus.be.server.user.domain.concert.entity.Concert;
+import kr.hhplus.be.server.user.domain.concert.entity.SeatMaster;
+import kr.hhplus.be.server.user.domain.seat.dto.response.FindAllSeatsResponse;
+import kr.hhplus.be.server.user.domain.seat.service.SeatService;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ControllerTest(controllers = SeatController.class)
+class SeatControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private SeatService seatService;
+
+    @Test
+    void findAllAvailableSeats() throws Exception {
+        //given
+        long concertId = 1L;
+        Concert concert = ConcertFixture.concert();
+        ReflectionTestUtils.setField(concert, "id", concertId);
+        SeatMaster seatMaster = ConcertFixture.seatMasterList().get(0);
+        FindAllSeatsResponse seats1 = FindAllSeatsResponse.of(concert, seatMaster);
+        List<FindAllSeatsResponse> expected = List.of(seats1);
+        when(seatService.findAllAvailableSeats(concertId)).thenReturn(expected);
+
+        //when & then
+        mockMvc.perform(
+                       get("/v1/concerts/{concertId}/seats", concertId)
+                               .contentType(MediaType.APPLICATION_JSON)
+               )
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.data[0].concertId").value(concertId))
+               .andExpect(jsonPath("$.data[0].seatId").value(seats1.seatId()))
+               .andExpect(jsonPath("$.data[0].seatNo").value(seats1.seatNo()))
+               .andExpect(jsonPath("$.data[0].status").value(seats1.status().name()))
+               .andExpect(jsonPath("$.data[0].concertDate").value(seats1.concertDate().toString()));
+    }
+}
